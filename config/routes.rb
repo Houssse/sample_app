5ad905c4386f37277e342e32397cf0bf831e935b
Houssse <yuriy.nikolaev.98@mail.ru>
2021-06-07 04:57:06 +0300

    Профиль пользывателя + подключения стиля

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
 end
