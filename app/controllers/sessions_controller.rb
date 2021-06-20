ba619c012c524dc1f0456fdb61d8c9fb10d6399c
Houssse <yuriy.nikolaev.98@mail.ru>
2021-06-20 15:15:01 +0300

    Готовый вход

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
       # Осуществить вход пользователя и перенаправление на страницу профиля.
     else
       flash.now[:danger] = 'Invalid email/password combination'
